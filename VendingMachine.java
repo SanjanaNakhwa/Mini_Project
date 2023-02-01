d294302b6c231aa5a9da9e03fff4fc8c2d3c19cb
Sanjana S Nakhwa <72541497+SanjanaNakhwa@users.noreply.github.com>
2023-02-01 17:15:40 -0500

    Delete VendingMachine.java

--- VendingMachine.java
+++
@@ -1,9 +0,0 @@
-
-public interface VendingMachine {
-
-     void displayProducts();
-     void selectProduct(int product);
-     void displayEnEnterCoinsMessage();
-     void enterCoins(int... coins);
-      void displayChangeMessage();
-}
